fea3cd1c5aae236243dbfe4235a7e9e01b7760bb
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-09-11 12:06:29 +0600

    zod validation implemented

---
+++ src/app/modules/product/product.validation.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const createProductValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string({ error: 'Name is required' })
+      .min(1, 'Name cannot be empty')
+      .trim(),
+    description: z
+      .string({ error: 'Description is required' })
+      .min(10, 'Description must be at least 10 characters long'),
+    price: z
+      .number({ error: 'Price is required' })
+      .min(0, 'Price cannot be negative'),
+    stock: z
+      .number({ error: 'Stock is required' })
+      .min(0, 'Stock cannot be negative')
+      .default(0),
+    category: z
+      .string({ error: 'Category is required' })
+      .min(1, 'Category cannot be empty')
+      .trim()
+      .toLowerCase(),
+  }),
+});
+
+export const ProductValidations = {
+  createProductValidationSchema,
+};
